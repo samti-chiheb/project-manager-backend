42f262699d14734dcf1864e27327ce9342805dd2
chihebgit <chiheb.samti@gmail.com>
2024-12-05 13:38:38 +0100

    add index, project controller & project routes

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// route imports
+import projectRoutes from "./routes/projectRoutes";
+
+// configurations
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("this is home route");
+});
+
+app.use('/projects', projectRoutes)
+
+// server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("====================================");
+  console.log(`Server running on port ${port}`);
+  console.log("====================================");
+});
