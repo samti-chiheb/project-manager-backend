b09cf3c229329f41c2ce0ab4ff5b1a0d31f19b64
chihebgit <chiheb.samti@gmail.com>
2024-12-11 01:06:48 +0100

    add team & user controllers & routes

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ import morgan from "morgan";
 import projectRoutes from "./routes/projectRoutes";
 import taskRoutes from "./routes/taskRoutes";
 import searchRoutes from "./routes/searchRoutes";
+import userRoutes from "./routes/userRoutes";
+import teamRoutes from "./routes/teamRoutes";
 
 // configurations
 dotenv.config();
@@ -28,8 +30,9 @@ app.get("/", (req, res) => {
 
 app.use('/projects', projectRoutes)
 app.use("/tasks", taskRoutes);
-
 app.use("/search", searchRoutes);
+app.use("/users", userRoutes);
+app.use("/teams", teamRoutes);
 
 // server
 const port = process.env.PORT || 3000;
